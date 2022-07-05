f64b19efd62b252739637d4541c3dfa63e17d916
Robin Drew <robindrew@users.noreply.github.com>
2022-07-05 15:40:21 +0100

    Refactored view video

--- src/main/java/com/robindrew/mediamanager/servlet/MediaController.java
+++ src/main/java/com/robindrew/mediamanager/servlet/MediaController.java
@@ -3,15 +3,25 @@ package com.robindrew.mediamanager.servlet;
 import static com.robindrew.mediamanager.component.file.cache.MediaFileCollection.splitToListWithType;
 import static com.robindrew.mediamanager.component.file.cache.MediaType.PHOTO;
 import static com.robindrew.mediamanager.component.file.cache.MediaType.VIDEO;
+import static org.springframework.http.MediaType.parseMediaType;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import com.google.common.io.ByteSource;
+import com.google.common.io.ByteStreams;
+import com.robindrew.common.http.MimeType;
+import com.robindrew.common.io.file.Files;
 import com.robindrew.mediamanager.component.file.cache.IMediaFile;
 import com.robindrew.mediamanager.component.file.cache.IMediaFileCollection;
 import com.robindrew.mediamanager.component.file.manager.IMediaFileManager;
@@ -52,5 +62,24 @@ public class MediaController {
 		dataMap.put("collections", collections);
 		dataMap.put("tagCache", fileTagCache);
 		return dataMap;
+	}
+
+	@GetMapping("/Videos/ViewVideo")
+	public ResponseEntity<StreamingResponseBody> viewVideo(@RequestParam int id) {
+
+		IMediaFile mediaFile = fileManager.getMediaFile(id);
+		File file = new File(fileManager.getRootDirectory(), mediaFile.getSourcePath());
+		ByteSource video = Files.asByteSource(file);
+		String type = MimeType.forExtension(mediaFile.getName());
+
+		// Streaming response
+		StreamingResponseBody responseBody = response -> {
+			try (InputStream from = video.openBufferedStream()) {
+				ByteStreams.copy(from, response);
+			} catch (Exception e) {
+			}
+		};
+
+		return ResponseEntity.ok().contentType(parseMediaType(type)).body(responseBody);
 	}
 }
